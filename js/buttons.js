08a3797379b96ba95c03f0918a1f0eee50ced631
Franco Calegari <francocalegari55@gmail.com>
2024-05-23 14:10:14 -0300

    imgjsfixes

--- js/buttons.js
+++ js/buttons.js
@@ -1,7 +1,7 @@
 const buttons = [
-    { button: document.getElementById('toggleButton1'), image: document.getElementById('buttonImage1'), stateKey: 'buttonState1', onImage: '/img/iconmiui12/sis/status_bar_toggle_bluetooth_on.png', offImage: '/img/iconmiui12/sis/status_bar_toggle_bluetooth_off.png' },
-    { button: document.getElementById('toggleButton2'), image: document.getElementById('buttonImage2'), stateKey: 'buttonState2', onImage: '/img/iconmiui12/sis/status_bar_toggle_wifi_on.png', offImage: '/img/iconmiui12/sis/status_bar_toggle_wifi_off.png' },
-    { button: document.getElementById('toggleButton3'), image: document.getElementById('buttonImage3'), stateKey: 'buttonState3', onImage: '/img/iconmiui12/sis/status_bar_toggle_data_on.png', offImage: '/img/iconmiui12/sis/status_bar_toggle_data_off.png' }
+    { button: document.getElementById('toggleButton1'), image: document.getElementById('buttonImage1'), stateKey: 'buttonState1', onImage: './img/iconmiui12/sis/status_bar_toggle_bluetooth_on.png', offImage: './img/iconmiui12/sis/status_bar_toggle_bluetooth_off.png' },
+    { button: document.getElementById('toggleButton2'), image: document.getElementById('buttonImage2'), stateKey: 'buttonState2', onImage: './img/iconmiui12/sis/status_bar_toggle_wifi_on.png', offImage: './img/iconmiui12/sis/status_bar_toggle_wifi_off.png' },
+    { button: document.getElementById('toggleButton3'), image: document.getElementById('buttonImage3'), stateKey: 'buttonState3', onImage: './img/iconmiui12/sis/status_bar_toggle_data_on.png', offImage: './img/iconmiui12/sis/status_bar_toggle_data_off.png' }
   ];
 
   buttons.forEach(buttonData => {
--- js/gamesback.js
+++ js/gamesback.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const section = document.getElementById('dynamicBackgroundSection');
     const images = [
-        '/img/gamesbackground/BloodStrikeBack1.jpg',
-        '/img/gamesbackground/call-of-duty-mobileimg1.jpg',
-        '/img/gamesbackground/MinecraftBack1.jpg',
-        '/img/gamesbackground/call-of-duty-mobileimg2.jpg',
-        '/img/gamesbackground/MinecraftBack2.jpg',
+        './img/gamesbackground/BloodStrikeBack1.jpg',
+        './img/gamesbackground/call-of-duty-mobileimg1.jpg',
+        './img/gamesbackground/MinecraftBack1.jpg',
+        './img/gamesbackground/call-of-duty-mobileimg2.jpg',
+        './img/gamesbackground/MinecraftBack2.jpg',
         // Añade más rutas de imágenes según sea necesario
     ];
     let currentIndex = 0;
